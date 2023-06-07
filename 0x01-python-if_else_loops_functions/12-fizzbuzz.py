15a2eb46d9eb04f810fc552eea8aca4d046233f3
hectoriabandini <abuhmoseji@gmail.com>
2023-06-07 03:36:12 -0700

    fizzbuzz

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def fizzbuzz():
+    for number in range(1, 101):
+        if number % 3 == 0:
+            print("fiz", end="")
+        elif number % 3 == 0 and number % 5 == 0:
+            print("fizzbuzz", end="")
+        elif number % 5 == 0:
+            print("buzz", end="")
+        else:
+            print("{}".format(number), end="")
