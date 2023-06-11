f6788aa29a51f9fcdd279371432e91b3fe456407
hectoriabandini <abuhmoseji@gmail.com>
2023-06-11 10:02:16 -0700

    task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+
 def element_at(my_list, idx):
-    if idx is < 0 or idx > len(my_list):
-        return None
+    """Element retrieval"""
+    if idx < 0 or idx >= len(my_list):
+        return(my_list[idx])
+    return my_list[idx]
