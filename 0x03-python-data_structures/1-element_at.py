8d4ca67dd87314bb9e39b68b7a32f38dd901f641
hectoriabandini <abuhmoseji@gmail.com>
2023-06-11 09:43:24 -0700

    added the range

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -2,6 +2,6 @@
 
 def element_at(my_list, idx):
     """ get an element to print"""
-    if idx < 0 or idx > (len(my_list) - 1):
+    if idx < 0 or idx >= (len(my_list) - 1):
         return None
     return (my_list[idx]) 
