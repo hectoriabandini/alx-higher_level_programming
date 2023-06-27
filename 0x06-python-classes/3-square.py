ea486ce0df45c1446cafae9e3b0726ae099bbaea
hectoriabandini <abuhmoseji@gmail.com>
2023-06-27 21:20:11 +0000

    modified 3square task

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,9 +1,8 @@
-#!/usr/bin/python3
 """Define a class Square."""
 
 
 class Square:
-	"""Represents a square."""
+    """Represents a square."""
 
     def __init__(self, size=0):
         """Initialize a new square.
