9ace31aa27f97cfc713fad7e966836c962004f90
hectoriabandini <abuhmoseji@gmail.com>
2023-07-11 20:11:46 +0000

    inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 
 def inherits_from(obj, a_class):
-    # Get the set of classes that obj is an instance of
-    obj_classes = set(type(obj).__mro__)
+	"""Get the set of classes that obj is an instance of """
+	obj_classes = set(type(obj).__mro__)
 
-    # Check if a_class is present in the set of classes
-    return any(a_class in cls.__mro__ for cls in obj_classes)
+	""" Check if a_class is present in the set of classes"""
+	return any(a_class in cls.__mro__ for cls in obj_classes)
