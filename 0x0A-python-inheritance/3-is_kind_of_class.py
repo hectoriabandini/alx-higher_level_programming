cd26ee612f00582811dcbd99c134e4c3ac5bdeb8
hectoriabandini <abuhmoseji@gmail.com>
2023-07-11 00:56:35 +0000

    iskind of a class

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,11 @@
+def is_kind_of_class(obj, a_class):
+    """ Check if the object is an instance of the specified class"""
+    if isinstance(obj, a_class):
+        return True
+    
+    """Check if the object is an instance of a class that inherited from the specified class"""
+    for cls in obj.__class__.mro():
+        if cls is a_class:
+            return True
+    
+    return False
