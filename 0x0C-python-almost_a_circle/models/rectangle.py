edb23ff8ebb3abcc04e55051d77adbf605f49393
hectoriabandini <abuhmoseji@gmail.com>
2023-07-17 10:48:45 +0000

    updated key word args

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -69,11 +69,13 @@ class Rectangle(Base):
 			print()
 		for x in range(self.height):
 			print(" " * self.x + "#" * self.width)
+		"""formated the strings"""
 	def __str__(self):
 		return "[Rectangle] ({}) {}/{} - {}/{}"\
 			.format(self.id, self.x, self.y, self.width,self.height)
 	
-	def update(self, *args):
+	def update(self, *args, **kwargs):
+		"""Adding keywords arg and possitional args"""
 		if len(args) >= 1:
 			self.id = args[0]
 		if len(args) >= 2:
@@ -84,3 +86,17 @@ class Rectangle(Base):
 			self.x = args[3]
 		if len(args) >= 5:
 			self.y = args[4]
+		if args:
+			if len(args) >= 1:
+                		self.id = args[0]
+			if len(args) >= 2:
+				self.width = args[1]
+			if len(args) >= 3:
+				self.height = args[2]
+			if len(args) >= 4:
+				self.x = args[3]
+			if len(args) >= 5:
+				self.y = args[4]
+		else:
+			for key, value in kwargs.items():
+				setattr(self, key, value)
