7fd98bf84f92354301b8e509f48be15f68e80479
hectoriabandini <abuhmoseji@gmail.com>
2023-07-17 08:56:23 +0000

    handled__str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -67,3 +67,6 @@ class Rectangle(Base):
 	def display(self):
 		for x in range(self.height):
 			print("#" * self.width)
+	def __str__(self):
+		return "[Rectangle] ({}) {}/{} - {}/{}"\
+			.format(self.id, self.x, self.y, self.width,self.height)
