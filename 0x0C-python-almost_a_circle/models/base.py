d599d73b4d73613e11946845a3e8d6f6acbb9faa
hectoriabandini <abuhmoseji@gmail.com>
2023-07-17 19:23:01 +0000

    updated json

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+import json
+
 class Base:
 	"""This class will be the base of all other classes in this project.
 	private class attribute __nb_objects
@@ -11,3 +13,10 @@ class Base:
 		else:
 			Base.__nb_objects += 1
 			self.id = Base.__nb_objects
+
+	@staticmethod
+	def to_json_string(list_dictionaries):
+		if list_dictionaries is None or len(list_dictionaries) == 0:
+			return "[]"
+		else:
+			return json.dumps(list_dictionaries)
