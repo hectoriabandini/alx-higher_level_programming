7fe90c8bdffc4f5802ca77f82e217d064bd687a1
hectoriabandini <abuhmoseji@gmail.com>
2023-07-17 20:34:39 +0000

    added comments to base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -8,6 +8,7 @@ class Base:
 	__nb_objects = 0
 	
 	def __init__(self, id=None):
+	"""The goal of it is to manage id attribute in all your future classes"""
 		if id is not None:
 			self.id = id
 		else:
@@ -16,15 +17,26 @@ class Base:
 
 	@staticmethod
 	def to_json_string(list_dictionaries):
+	"""json method converting list_dictionaries to json strings"""
 		if list_dictionaries is None or len(list_dictionaries) == 0:
 			return "[]"
 		else:
 			return json.dumps(list_dictionaries)
 	@classmethod
 	def save_to_file(cls, list_objs):
+	""""The goal of it is to manage id attribute in all your future 
+	classes and to avoid duplicating the same code (by extension, same bugs)
+	"""
 		if list_objs is None:
 			list_objs = []
 		filename = cls.__name__ + ".json"
 		json_data = cls.to_json_string([obj.to_dictionary() for obj in list_objs])
 		with open(filename, 'w') as file:
 			file.write(json_data)
+	@staticmethod
+	def from_json_string(json_string):
+	"""Converting json to a string"""
+		if json_string is None or len(json_string) == 0:
+			return "[]"
+		else:
+			return json.loads(json_string)
