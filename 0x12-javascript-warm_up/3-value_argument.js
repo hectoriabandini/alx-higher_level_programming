7cf9a3a6e7269defdc7371f956083a7bd265fc36
hectoriabandini <abuhmoseji@gmail.com>
2023-11-14 08:25:26 -0800

    argsfound3

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-
-// Extract command-line arguments excluding the first two elements
-const args = argv.slice(2);
-
-// Check if any arguments are present and print the first one or "No argument"
-console.log(args[0] !== undefined ? args[0] : "No argument");
+if (process.argv[2] === undefined) {
+  console.log('No argument');
+} else {
+  console.log(process.argv[2]);
+}
