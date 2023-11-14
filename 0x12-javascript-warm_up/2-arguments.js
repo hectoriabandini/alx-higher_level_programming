2e579b8ff38aa092e64d986a84c8914f2adccceb
hectoriabandini <abuhmoseji@gmail.com>
2023-11-14 08:17:06 -0800

    argsfound

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,12 +1,8 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-// Extract command-line arguments excluding the first two elements
-const args = argv.slice(2);
-// Check the number of arguments and print messages accordingly
-if (args.length === 0) {
-  console.log("No argument");
-} else if (args.length === 1) {
-  console.log("Argument found");
+if (process.argv.length === 2) {
+  console.log('No argument');
+} else if (process.argv.length === 3) {
+  console.log('Argument found');
 } else {
-  console.log("Arguments found");
+  console.log('Arguments found');
 }
