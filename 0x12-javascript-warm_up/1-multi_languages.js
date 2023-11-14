78dd88b392f1b685168e6def7e55706de83ab457
hectoriabandini <abuhmoseji@gmail.com>
2023-11-14 06:11:24 -0800

    print 3lines

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const myC = 'C is fun';
+const myPython = 'Python is cool';
+const Jscript = 'JavaScript is amazing';
+console.log(myC);
+console.log(myPython);
+console.log(Jscript);
