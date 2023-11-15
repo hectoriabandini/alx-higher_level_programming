5a16409cbb0f50ab6574a2a361267e73d3d934d0
hectoriabandini <abuhmoseji@gmail.com>
2023-11-15 12:19:14 -0800

    other tasks

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const myLine = ['C is fun','Python is cool','JavaScript is amazing'];
-for (let i = 0; i < myLine.length; i++){
-console.log(myLine[i]);
+for (const line of ['C is fun', 'Python is cool', 'JavaScript is amazing']) {
+  console.log(line);
 }
