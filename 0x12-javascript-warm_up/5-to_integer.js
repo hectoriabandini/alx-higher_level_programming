6cbc14db98d29d456616d382b6af038fc9ee8ce0
hectoriabandini <abuhmoseji@gmail.com>
2023-11-15 00:33:58 -0800

    parse

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,8 +2,9 @@
 const { argv } = require("node:process");
 const args = argv.slice(2);
 args.forEach((arg) => {
-  const num = parseFloat(arg);
+  const num = parseInt(arg);
   if (!isNaN(num)) {
     console.log('My number:' + num);
-  }
+  }else{
+  console.log('Not a number')}
 });
