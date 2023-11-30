54c85c25dfb379950be94e9b92ec3b65453c5ad5
hectoriabandini <abuhmoseji@gmail.com>
2023-11-30 12:36:24 -0800

    0body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-# Check if a URL is provided as an argument
+# Check if URL is provided as argument
 if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
+  echo "Usage: $0 <URL>"
+  exit 1
 fi
 
-# Use curl to send a request and display the size of the response body in bytes
-response_size=$(curl -sI "$1" | grep -i "Content-Length" | awk '{print $2}' | tr -d '\r\n')
-echo "$response_size"
-
+# Send request and display size of response body
+curl -sI "$1" | grep -i Content-Length | awk '{print $2}'
